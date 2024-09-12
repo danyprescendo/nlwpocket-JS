7d637170da30b165a52d46132097e1f0193f5d2a
danyprescendo <danypablo08@hotmail.com>
2024-09-12 20:08:02 -0300

    :sparkles: feat: fundamentos 004 e 005

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const start = () => {
-  let count = 1;
-  while (count <= 10) {
-    console.log(count);
-    count++;
+  while (true) {
+    let option = 'Sair';
+    switch (option) {
+      case 'cadastrar':
+        console.log('Vamos cadastrar.');
+        break;
+      case 'Listar':
+        console.log('Vamos listar.');
+        break;
+      case 'Sair':
+        return;
+    }
   }
 };
 
